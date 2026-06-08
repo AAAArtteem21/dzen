33eb04d76e198227ecc68a423e6ec354345db3cd
sskt98 <sskt0965@gmail.com>
2026-06-08 12:15:44 +0300

    fix media

--- spa/spa/settings.py
+++ spa/spa/settings.py
@@ -117,5 +117,4 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'https://res.cloudinary.com/dqvwmner2/'
