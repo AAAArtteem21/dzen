40a1749b879a309be5e8edf710024a7012ce25ac
sskt98 <sskt0965@gmail.com>
2026-06-08 11:23:55 +0300

    fix redis

--- spa/spa/settings.py
+++ spa/spa/settings.py
@@ -63,7 +63,10 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [config('REDIS_URL', default='redis://127.0.0.1:6379')],
+            'hosts': [{
+                'address': config('REDIS_URL', default='redis://127.0.0.1:6379'),
+                'ssl': True,
+            }],
         },
     },
 }
